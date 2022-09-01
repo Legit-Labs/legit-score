ff5c9da30ab544b086cfeef78669f22ed50bd191
gal-legit <gal@legitsecurity.com>
2022-09-01 10:50:19 +0300

    implement

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/legit-labs/legit-score
+
+go 1.18
--- predicate.go
+++ predicate.go
@@ -1,7 +1,32 @@
 package legitscore
 
+import (
+	"fmt"
+	"time"
+)
+
 type LegitScorePredicate struct {
 	Repository string  `json:"repository"`
 	Timestamp  string  `json:"timestamp"`
 	Score      float64 `json:"score"`
+}
+
+func FetchScore(repo string, api_token string) *LegitScorePredicate {
+	return &LegitScorePredicate{
+		Repository: repo,
+		Timestamp:  time.Now().Format(time.RFC1123),
+		Score:      7, // TODO query Legit
+	}
+}
+
+func (l *LegitScorePredicate) Verify(repo string, min_score float64) error {
+	if l.Repository != repo {
+		return fmt.Errorf("predicate repository (%v) does not match expected repository (%v)", l.Repository, repo)
+	}
+
+	if l.Score < min_score {
+		return fmt.Errorf("predicate score (%v) is below the min score (%v)", l.Score, min_score)
+	}
+
+	return nil
 }
